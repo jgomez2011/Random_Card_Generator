027a2e2cc5b2bf477379c2fce8f7277eae08e156
jgomez2011 <jesus.gomez0911@gmail.com>
2023-07-27 20:53:58 +0000

    Cards

--- src/app.js
+++ src/app.js
@@ -1,11 +1,56 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function RandomValue() {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let finalValue = Math.floor(Math.random() * values.length);
+  return values[finalValue];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomSuit() {
+  let suit = ["♦", "♥️", "♠", "♣"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let card = document.querySelector("#card");
+
+let finalSuit = randomSuit();
+let finalValue = RandomValue();
+
+card.style.color = `${finalSuit == "♦" || finalSuit == "♥️" ? "red" : "black"}`;
+
+let topSuit = document.createElement("div");
+card.appendChild(topSuit);
+topSuit.style.height = "100px";
+topSuit.style.fontsize = "100px";
+topSuit.style.paddingleft = "20px";
+topSuit.innerHTML = `${finalSuit}`;
+
+let midValue = document.createElement("div");
+card.appendChild(midValue);
+midValue.style.height = "300px";
+midValue.style.fontsize = "150px";
+midValue.style.display = "flex";
+midValue.style.justifyContent = "center";
+midValue.style.alignItems = "center";
+midValue.innerHTML = `${finalValue}`;
+
+let bottomSuit = document.createElement("div");
+card.appendChild(bottomSuit);
+bottomSuit.style.height = "100px";
+bottomSuit.style.fontsize = "100px";
+bottomSuit.style.paddingleft = "20px";
+bottomSuit.style.transform = "rotate(180deg)";
+bottomSuit.innerHTML = `${finalSuit}`;
